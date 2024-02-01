582363bf0c75624f573763ff51a18c64f62675df
Nathan Chen <113192742+seribical@users.noreply.github.com>
2024-01-31 21:51:38 -0500

    im goated

--- awgg/js/main.js
+++ awgg/js/main.js
@@ -1,5 +1,7 @@
 import "../style.css"
 
+const gamerecord = []
+
 function reverse(word){
     let split = word.split("")
     let reversed = split.reverse()
@@ -10,47 +12,53 @@ function reverse(word){
 
 async function genword(){
     try{
-        let call = await fetch(`https://random-word-api.herokuapp.com/word?length=5&lang=en`);
+        let call = await fetch("https://random-word-api.herokuapp.com/word?length=5&lang=en");
         let word = await call.json();
         let ans = String(word[0])
         game(reverse(ans),ans)
     }
-    catch{
-        console.log("caugh")
-    }
+    catch{console.log("fix your code")}
+}
 
+function setstage(){
+    document.querySelector("#app").innerHTML=""
+    for(let i = 1; i < 7; i++){
+        document.querySelector("#app").insertAdjacentHTML(
+            "beforeend",
+            `<div id="column${i}"></div>`)
+        for(let j = 0; j < 5; j++){
+            document.querySelector(`#column${i}`).insertAdjacentHTML(
+                "afterbegin",
+                `<div id="box"></div>`)
+        }
+    }
+    document.querySelector("#app").insertAdjacentHTML(
+        "beforeend",
+        `<input type="text" id="input" placeholder="Type a word">`
+    )
 }
 
 function game(answer,answered){
     let tries = 1
-    document.querySelector("#input").addEventListener("keyup",(event)=>{
+    setstage()
+    document.querySelector("#input").addEventListener("keypress",(event)=>{
         if(event.key==="Enter"){
             let guess = reverse(document.querySelector("#input").value)
             if (guess.length === 5){
-                console.log(tries)
-                console.log(answer)
                 document.querySelector(`#column${tries}`).innerHTML=""
                 document.querySelector("#input").value=""
                 if(guess===answer){
                     checkletter(guess,answer,tries)
-                    document.querySelector("#input").remove()
-                    document.querySelector(`#app`).insertAdjacentHTML(
-                        "beforeend",
-                        `<p>you winned</p>`)
+                    status(answered, tries, "lost")
                 }else if(tries===6){
                     checkletter(guess,answer,tries)
-                    document.querySelector("#input").remove()
-                    document.querySelector(`#app`).insertAdjacentHTML(
-                        "beforeend",
-                        `<p>you lose the word was ${answered}</p>`)
-                }else{
-                    checkletter(guess,answer,tries)
-                           }
+                    status(answered, tries, "lost")
+                }else{checkletter(guess,answer,tries)}
                   tries++
             }else{}
+            document.querySelector("#again").addEventListener("click", function(){genword()})
         }
-    }
-    )
+    })
 }
 
 function checkletter(guess,answer,tries){
@@ -58,20 +66,35 @@ function checkletter(guess,answer,tries){
         if(guess[i]===answer[i]){
             document.querySelector(`#column${tries}`).insertAdjacentHTML(
                 "afterbegin",
-                `<div id="boxyes">${guess[i]}</div>`
-            )
+                `<div id="boxyes">${guess[i]}</div>`)
         }else if(answer.includes(guess[i])===true){
             document.querySelector(`#column${tries}`).insertAdjacentHTML(
                 "afterbegin",
-                `<div id="boxmaybe">${guess[i]}</div>`
-            )
+                `<div id="boxmaybe">${guess[i]}</div>`)
         }else if(answer.includes(guess[i])===false){
             document.querySelector(`#column${tries}`).insertAdjacentHTML(
                 "afterbegin",
-                `<div id="boxno">${guess[i]}</div>`
-            )
+                `<div id="boxno">${guess[i]}</div>`)
         }
+    }
 }
+
+function status(answered, tries, status){
+    document.querySelector("#input").remove()
+    document.querySelector(`#app`).insertAdjacentHTML(
+        "beforeend",
+        `<p>you ${status} the word was ${answered}</p>
+        <button id="again">play again</button>`)
+        gamerecord.push(answered, tries, status)
+        record()
+}
+
+function record(){
+    const index=gamerecord.length
+    const text=(gamerecord[index-1]+" "+gamerecord[index-3]+" "+"in"+" "+gamerecord[index-2]+" "+"tries")
+    document.querySelector("#record").insertAdjacentHTML(
+        "afterbegin",
+        `<p>${text}<p>`)
 }
 
 genword();
